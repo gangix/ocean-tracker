4979412911871f27c7e252fd1d590c5794cd616a
olcay bilir <olcay.bilir@xing.com>
2024-02-17 20:45:33 +0100

    Add scheduler some logs

--- src/main/java/com/cleanhub/cleanhubtracker/job/CustomerOrderScheduler.java
+++ src/main/java/com/cleanhub/cleanhubtracker/job/CustomerOrderScheduler.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import com.cleanhub.cleanhubtracker.client.CustomerInformationClient;
 import com.cleanhub.cleanhubtracker.client.OrderInformationClient;
@@ -18,34 +19,35 @@ import jakarta.transaction.Transactional;
 public class CustomerOrderScheduler {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerOrderScheduler.class);
-	
-	
+
 	private final OrderDetailRepository orderDetailRepository;
 	private final CustomerInformationClient customerInformationClient;
 	private final OrderInformationClient orderInformationClient;
-	
-	
-	public CustomerOrderScheduler(OrderDetailRepository orderDetailRepository, CustomerInformationClient customerInformationClient,
-			OrderInformationClient orderInformationClient) {
+
+	public CustomerOrderScheduler(OrderDetailRepository orderDetailRepository,
+			CustomerInformationClient customerInformationClient, OrderInformationClient orderInformationClient) {
 		this.orderDetailRepository = orderDetailRepository;
 		this.customerInformationClient = customerInformationClient;
 		this.orderInformationClient = orderInformationClient;
 	}
 
-
 	@Scheduled(cron = "${cleanhub.api.feed.cron}")
 	@Transactional
 	public void emitAndPersistOrderDetail() {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start("emitAndPersistOrderDetail");
 		try {
-			customerInformationClient.getCustomers()
-				.forEach(customerDetail -> {
-					var orderDetail = orderInformationClient.getOrderDetail(customerDetail.landingPageRoute());
-					orderDetail.setCompanyName(customerDetail.companyName());
-					orderDetailRepository.save(orderDetail);
-			});	
-		} 
-		catch (FeignClientException feignClientException) {
+			customerInformationClient.getCustomers().forEach(customerDetail -> {
+				var orderDetail = orderInformationClient.getOrderDetail(customerDetail.landingPageRoute());
+				orderDetail.setCompanyName(customerDetail.companyName());
+				orderDetailRepository.save(orderDetail);
+			});
+		} catch (FeignClientException feignClientException) {
 			LOGGER.error("Feign client exception occured", feignClientException);
 		}
+
+		stopWatch.stop();
+		LOGGER.info("Time taken to complete {} : {}", stopWatch.lastTaskInfo(), stopWatch.getTotalTimeSeconds());
+
 	}
 }
